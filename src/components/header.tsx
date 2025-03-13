e1a0047a40c7cae847f054e357570e734e00d4b0
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-03-13 08:12:34 +0100

    [feature] implement internationalization support with next-intl and add header component

---
+++ src/components/header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { LanguageSelector } from '@/components/language-selector';
+import { useTranslations } from 'next-intl';
+
+export const Header: React.FC = () => {
+	return (
+		<header className="text-primary-secondary py-2 px-2 sm:py-4 sm:px-6 flex items-center justify-between border-b-2 border-0 select-none">
+			<div className="flex items-center gap-2 sm:gap-4 p-1 sm:p-3">
+				<img src="https://epfl-si.github.io/elements/svg/epfl-logo.svg" alt="LOGO" className="h-4 sm:h-7" />
+				<span className="border-l-2 border-solid sm:h-6 h-4 w-1 border-gray-300"></span>
+				<h1 className="text-base sm:text-2xl font-bold -ml-1 sm:ml-0">WP-Kleenex</h1>
+			</div>
+			<div className="flex items-center gap-2 sm:gap-4">
+				<LanguageSelector />
+			</div>
+		</header>
+	);
+};
