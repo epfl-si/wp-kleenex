8cc903f5849cdb9d5efc21660494a0eda95cb8d4
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-04-11 22:07:27 +0200

    [feature] add createSite and getSites functions for site management

---
+++ src/services/site.ts
@@ -0,0 +1,50 @@
+import { customObjectsApi, getNamespace } from '@/lib/api';
+import { APIError } from '@/types/error';
+import { Site } from '@/types/site';
+import { auth } from './auth';
+
+export async function createSite(site: Site): Promise<APIError> {
+	try {
+		const session = await auth();
+		if (!session) {
+			return { status: 401, message: 'Unauthorized' };
+		}
+
+		const namespace = await getNamespace();
+		await customObjectsApi.createNamespacedCustomObject({
+			group: 'wordpress.epfl.ch',
+			version: 'v2',
+			namespace: namespace,
+			plural: 'wordpresssites',
+			body: site,
+		});
+
+		return { status: 201, message: 'Created' };
+	} catch (error) {
+		console.error('Error creating site', error);
+		return { status: 500, message: 'Internal Server Error' };
+	}
+}
+
+export async function getSites(): Promise<Site[] | null> {
+	try {
+		const session = await auth();
+		if (!session || !session.user.role || session.user.role !== 'admin') {
+			return null;
+		}
+
+		const namespace = await getNamespace();
+
+		const response: any = await customObjectsApi.listNamespacedCustomObject({
+			group: 'wordpress.epfl.ch',
+			version: 'v2',
+			namespace: namespace,
+			plural: 'wordpresssites',
+		});
+
+		return response?.items.map((site: any) => site.spec) ?? null;
+	} catch (error) {
+		console.error('Error getting sites', error);
+		return null;
+	}
+}
