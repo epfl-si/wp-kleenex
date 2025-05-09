95008b7dec30769cbb330b307754e3a2b34b54b8
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-05-09 14:07:01 +0200

    [feature] refactor Site interface and update wordpressSiteSchema for improved structure

--- src/types/site.ts
+++ src/types/site.ts
@@ -1,18 +1,28 @@
+import { z } from 'zod';
 export interface Site {
 	hostname: string;
 	path: string;
-	owner: {
-		epfl: {
-			userId: string;
-		};
-	};
+	owner: string;
+	creationTimestamp?: string;
+	expirationTimestamp?: string;
+	id: string;
+	expiration: 10800 | 21600 | 43200;
+	type: 'basic' | 'blank' | 'formation' | 'copy';
 	wordpress: {
 		debug: boolean;
-		plugins: string[];
 		theme: string;
 		languages: string[];
 		title: string;
 		tagline: string;
-		type?: 'basic' | 'blank' | 'formation' | 'copy';
 	};
 }
+
+export const wordpressSiteSchema = z.object({
+	title: z.string().min(1),
+	tagline: z.string().min(1),
+	theme: z.enum(['wp-theme-2018', 'wp-theme-light', 'epfl-master', 'epfl-blank']),
+	languages: z.array(z.string().regex(/^[a-z]{2}$/)).min(1),
+	type: z.enum(['basic', 'blank', 'formation', 'copy']),
+	expiration: z.union([z.literal(10800), z.literal(21600), z.literal(43200)]),
+	debug: z.boolean(),
+});
