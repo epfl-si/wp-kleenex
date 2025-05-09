c22bda9445d8545fb71175cf6d9e196ca575c1de
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-05-09 14:07:21 +0200

    [refactor] Sidebar component to improve link styling and hover effects

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -13,16 +13,16 @@ export const Sidebar: React.FC<{ session: Session }> = ({ session }) => {
 	return (
 		<aside className="w-1/6 space-y-2 border-r p-4">
 			<nav className="space-y-2 w-full">
-				<Link href="/" className={cn('px-6 py-3 rounded-lg flex text-primary-secondary font-medium w-full items-center justify-start gap-2 hover:bg-gray-200 hover:bg-opacity-50', pathname === '/' && 'text-red-500')}>
+				<Link href="/" className={cn('px-6 py-3 rounded-lg flex text-primary-secondary font-medium w-full items-center justify-start gap-2', pathname === '/' ? 'text-primary font-semibold' : 'hover:bg-muted/80')}>
 					<Globe className="w-6 h-6 inline-block" />
 					{t('home')}
 				</Link>
-				<Link href="/new" className={cn('px-6 py-3 rounded-lg flex text-primary-secondary font-medium w-full items-center justify-start gap-2 hover:bg-gray-200 hover:bg-opacity-50', pathname === '/new' && 'text-red-500')}>
+				<Link href="/new" className={cn('px-6 py-3 rounded-lg flex text-primary-secondary font-medium w-full items-center justify-start gap-2', pathname === '/new' ? 'text-primary font-semibold' : 'hover:bg-muted/80')}>
 					<Plus className="w-6 h-6 inline-block" />
 					{t('new')}
 				</Link>
 				{session.user.role === 'admin' && (
-					<Link href="/all" className={cn('px-6 py-3 rounded-lg flex text-primary-secondary font-medium w-full items-center justify-start gap-2 hover:bg-gray-200 hover:bg-opacity-50', pathname === '/all' && 'text-red-500')}>
+					<Link href="/all" className={cn('px-6 py-3 rounded-lg flex text-primary-secondary font-medium w-full items-center justify-start gap-2', pathname === '/all' ? 'text-primary font-semibold' : 'hover:bg-muted/80')}>
 						<GalleryVerticalEnd className="w-6 h-6 inline-block" />
 						{t('all')}
 					</Link>
